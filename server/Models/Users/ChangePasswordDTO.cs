6bd61ce0fa7b1175d173c206a5486196843d804c
ritz10001 <ritvik11prakash@gmail.com>
2025-04-21 15:20:01 -0500

    Added UpdateUserProfile

---
+++ server/Models/Users/ChangePasswordDTO.cs
@@ -0,0 +1,11 @@
+
+
+namespace server.Models.Users
+{
+    public class ChangePasswordDTO
+    {
+        public string CurrentPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
